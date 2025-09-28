c251dbf694f77c82fb0f0eac50a0b70aa59e7446
Hoang Cong Minh <hoangcongminh0912@gmail.com>
2025-09-29 05:43:11 +0700

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,1 +1,51 @@
+# ==========================
+# 1. INSTALL & LOAD LIBRARIES 
+# ==========================
+install_if_missing <- function(pkg){
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+        install.packages(pkg, dependencies = TRUE)
+    }
+    require(pkg, character.only = TRUE) 
+}
 
+# List of required packages
+packages_minimal <- c(
+    "dplyr",      
+    "rsample",    
+    "yardstick",  
+    "glmnet",     
+    "mgcv",       
+    "janitor",    
+    "purrr",     
+    "recipes",    
+    "parsnip",   
+    "workflows",  
+    "openxlsx",   
+    "skimr",      
+    "RANN"        
+)
+
+# checking, installing and loading of packages
+cat("⏳ Checking, installing, and loading necessary packages...\n")
+invisible(lapply(packages_minimal, install_if_missing))
+cat("✅ All essential packages loaded successfully.\n")
+
+# Declare libraries
+library(dplyr)
+library(rsample)
+library(yardstick)
+library(glmnet)
+library(mgcv)
+library(openxlsx)
+library(janitor)
+library(purrr)
+library(recipes)
+library(RANN)
+library(skimr) 
+library(parsnip)
+library(workflows)
+
+
+set.seed(123)
+
+# ... (Phần còn lại của code từ Mục 2 đến Mục 6 được giữ nguyên)
