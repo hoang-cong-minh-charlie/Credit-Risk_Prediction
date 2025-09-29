a89fc3618d73867909cd3194c8d0e4a3cf52759b
Hoang Cong Minh <hoangcongminh0912@gmail.com>
2025-09-29 11:02:20 +0700

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -30,20 +30,6 @@ cat("⏳ Checking, installing, and loading necessary packages...\n")
 invisible(lapply(packages_minimal, install_if_missing))
 cat("✅ All essential packages loaded successfully.\n")
 
-# Declare libraries
-library(dplyr)
-library(rsample)
-library(yardstick)
-library(glmnet)
-library(mgcv)
-library(openxlsx)
-library(janitor)
-library(purrr)
-library(recipes)
-library(RANN)
-library(skimr) 
-library(parsnip)
-library(workflows)
 
 
 set.seed(123)
